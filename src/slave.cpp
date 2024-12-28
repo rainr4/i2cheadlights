93f530acec3fc310105c4685eb11d4fa06eadb29
rainr4 <157737701+rainr4@users.noreply.github.com>
2024-12-27 22:20:53 -0600

    modified:   src/slave.cpp

--- src/slave.cpp
+++ src/slave.cpp
@@ -11,21 +11,42 @@ constexpr static const int max_size = 1024;
 static uint8_t command_buffer[max_size];
 static int command = 0; // 0 is stop 
 
-static void on_receive_command(int len) {
-    if(len>0 && len<=max_size) {
-        Wire.readBytes(command_buffer,len);
-        command = *command_buffer;
-    }
-}
 template<typename T>
 static void read_command(T* out_cmd) {
     memcpy(out_cmd,command_buffer+1,sizeof(T));
 }
 
+void execute_fade(const cmd_fade_t* fade_cmd) {
+    //need to figure out code for fade
+}
+
+static void on_receive_command(int len) {
+    if(len>0 && len<=max_size) {
+        Wire.readBytes(command_buffer,len);
+        command = *command_buffer;
+
+        switch (command)
+        {
+        case CMD_FADE:
+          cmd_fade_t fade_cmd;
+          read_command(&fade_cmd);
+          execute_fade(&fade_cmd);
+          break;
+        
+        default:
+          Serial.println("Unknown command. ");
+          break;
+        }
+    }
+}
+
+
 void setup() {
   Serial.begin(115200);
+  Wire.begin(I2C_DEV_ADDR, 22, 23, 100*1000);
+  Wire.onReceive(on_receive_command);
+  Serial.println("Slave rdy");
   Serial.setDebugOutput(true);
-  Wire.begin(0x40, 22, 23, 100*1000);
 }
 
 void loop() {
